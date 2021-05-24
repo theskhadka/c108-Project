aaa6f2ea3d0a54a64bd17401ec760b4098be0f60
theskhadka <69537395+theskhadka@users.noreply.github.com>
2021-05-24 12:09:55 -0700

    Update and rename weight.py to ratings.py

--- ratings.py
+++ ratings.py
@@ -2,5 +2,5 @@ import pandas as pd
 import csv
 import plotly.figure_factory as ff
 df = pd.read_csv("bellcurve.csv")
-fig = ff.create_distplot([df["Weight(Pounds)"].tolist()], ["Weight"], show_hist = False)
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"], show_hist = False)
 fig.show()
