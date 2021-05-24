5a9f1eaea3da3a62b83972820fba41f3fde8992c
theskhadka <69537395+theskhadka@users.noreply.github.com>
2021-05-24 12:09:22 -0700

    Update height.py

--- height.py
+++ height.py
@@ -2,5 +2,5 @@ import pandas as pd
 import csv
 import plotly.figure_factory as ff
 df = pd.read_csv("bellcurve.csv")
-fig = ff.create_distplot([df["Height(Inches)"].tolist()], ["Height"], show_hist = False)
+fig = ff.create_distplot([df["Mobile Brand"].tolist()], ["Mobile Brand"], show_hist = False)
 fig.show()
